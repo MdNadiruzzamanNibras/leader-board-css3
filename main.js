88f31cb8d487a5d08f3aec7c50487761fa1f9e5a
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-02-15 11:19:40 +0600

    color change

---
+++ main.js
@@ -0,0 +1,3 @@
+document.getElementById("top-player-color").style.color='red';
+document.getElementById("top-blog-color").style.color='pink';
+document.getElementById("top-bgColor").style.backgroundColor='rgb(94 234 212)';
